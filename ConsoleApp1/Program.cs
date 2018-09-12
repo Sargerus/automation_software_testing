0f8d5e132e55b9a2ce50f9e6bc53d68259b3b6bf
Sargerus <pavel.kulich@leverx.com>
2018-09-12 20:04:08 +0300

    Add calculator project
    -Add .gitignore
    -Bug fix
    -Release version 1.1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface ICalculatable
+{
+    double Calculate(double a, double b);
+}
+
+public class Sum : ICalculatable
+{
+    double ICalculatable.Calculate(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+public class Substract : ICalculatable
+{
+    double ICalculatable.Calculate(double a, double b)
+    {
+        return a - b;
+    }
+}
+
+public class Divide : ICalculatable
+{
+    double ICalculatable.Calculate(double a, double b)
+    {
+        return a / b;
+    }
+}
+
+public class Multiply : ICalculatable
+{
+    double ICalculatable.Calculate(double a, double b)
+    {
+        return a * b;
+    }
+}
+
+
+
+class Program
+    {
+
+    private static Dictionary<char, ICalculatable> Dictionary = new Dictionary<char, ICalculatable>();
+
+        static void Main(string[] args)
+        {
+
+        double a = 0, b = 0;
+        char operation;
+
+        FillDictionary();
+
+        try
+        {
+            Console.WriteLine("Enter first digit: ");
+
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter operation: ");
+
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter second digit: ");
+
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Result: " + Dictionary[operation].Calculate(a, b));
+        }
+
+        catch
+        {
+            Console.WriteLine("Error: Invalid Input");
+        }
+
+        }
+    private static void FillDictionary()
+    {
+        Dictionary.Add('+', new Sum());
+        Dictionary.Add('-', new Substract());
+        Dictionary.Add('*', new Multiply());
+        Dictionary.Add('/', new Divide());
+    }
+
+    }
+
